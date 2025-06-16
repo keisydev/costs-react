a1009c69a11c663ab60434bddf801060dc547b5c
Keisy Rathlef <keisy.dev@gmail.com>
2025-06-16 12:19:39 +0100

    Adaptação GitHub pages
     Adapta projeto para deploy estático sem backend

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
